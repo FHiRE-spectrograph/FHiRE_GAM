6500253600449228d7baeb6746df4a7c92d66675
FHiRE <fhire@fhire_GAM.uwyo.edu>
2020-10-07 17:24:59 -0600

    Tested and is working, yay! Haven't checked frame dimensions yet, but those should be fine? Need to clean up layout to fit text and add label to show set temperature.

--- ZWOguiding_camera.py
+++ ZWOguiding_camera.py
@@ -1,16 +1,18 @@
 from PyQt5 import QtGui,QtCore,QtWidgets,uic
 from PyQt5.QtCore import QThread,pyqtSignal
+import time
 
 import zwocamerawindow #imports PyQt ZWO camera settings window
-import GUI_pyqt5.MainUiClass as main
 
 class ZWOCameraWindow(QtWidgets.QMainWindow, zwocamerawindow.Ui_ZWOcamera):
-	def __init__(self, parent=None):
-		super(ZWOCameraWindow,self).__init__(parent)
+	def __init__(self,main):
+		self.main = main
+		super(ZWOCameraWindow,self).__init__(main)
 		self.setupUi(self)
 
 		self.pb_cooler.setCheckable(True)
 		self.pb_cooler.setStyleSheet("background-color: #c9d1d1")
+		self.pb_cooler.setText("OFF")
 		self.pb_cooler.setStyleSheet("QPushButton:checked {background-color: #95fef9}")
 
 		self.sb_binningx.setRange(0,100)
@@ -19,43 +21,67 @@ class ZWOCameraWindow(QtWidgets.QMainWindow, zwocamerawindow.Ui_ZWOcamera):
 		self.sb_offset.setRange(0,1000)
 		self.sb_bandwidth.setRange(0,500)
 
-		self.pb_cooler.pressed.connect(lambda:self.cool)
-		self.sb_binningx.valueChanged.connect(main.threadclass.update_xbin)
-		self.sb_binningy.valueChanged.connect(main.threadclass.update_ybin)
-		self.sb_gain.valueChanged.connect(main.threadclass.update_gain)
-		self.sb_offset.valueChanged.connect(main.threadclass.update_offset)
-		self.sb_bandwidth.valueChanged.connect(main.threadclass.update_band)
+		self.pb_cooler.pressed.connect(self.cool)
+		self.sb_binningx.valueChanged.connect(self.main.threadclass.update_xbin)
+		self.sb_binningy.valueChanged.connect(self.main.threadclass.update_ybin)
+		self.sb_gain.valueChanged.connect(self.main.threadclass.update_gain)
+		self.sb_offset.valueChanged.connect(self.main.threadclass.update_offset)
+		self.sb_bandwidth.valueChanged.connect(self.main.threadclass.update_band)
 
-		self.rb_light.toggled.connect(main.threadclass.frametype_light)
-		self.rb_dark.toggled.connect(main.threadclass.frametype_dark)
-		self.rb_flat.toggled.connect(main.threadclass.frametype_flat)
-		self.rb_bias.toggled.connect(main.threadclass.frametype_bias)
+		self.rb_light.toggled.connect(self.frametype)
+		self.rb_dark.toggled.connect(self.frametype)
+		self.rb_flat.toggled.connect(self.frametype)
+		self.rb_bias.toggled.connect(self.frametype)
 
-		self.rb_8bit.toggled.connect(main.threadclass.bit_eight)
-		self.rb_16bit.toggled.connect(main.threadclass.bit_sixteen)
+		self.rb_8bit.toggled.connect(self.bit)
+		self.rb_16bit.toggled.connect(self.bit)
 
-		self.ln_framex.returnPressed.connect(lambda:main.threadclass.update_xframe(int(self.ln_framex.text())))
-		self.ln_framey.returnPressed.connect(lambda:main.threadclass.update_yframe(int(self.ln_framey.text())))
-		self.ln_leftmost.returnPressed.connect(lambda:main.threadclass.update_xposition(int(self.ln_leftmost.text())))
-		self.ln_topmost.returnPressed.connect(lambda:main.threadclass.update_yposition(int(self.ln_topmost.text())))
+		self.ln_framex.returnPressed.connect(lambda:self.main.threadclass.update_xframe(int(self.ln_framex.text())))
+		self.ln_framey.returnPressed.connect(lambda:self.main.threadclass.update_yframe(int(self.ln_framey.text())))
+		self.ln_leftmost.returnPressed.connect(lambda:self.main.threadclass.update_xposition(int(self.ln_leftmost.text())))
+		self.ln_topmost.returnPressed.connect(lambda:self.main.threadclass.update_yposition(int(self.ln_topmost.text())))
 
-		self.pb_setTemp.pressed.connect(lambda:main.threadclass.change_temp(float(self.ln_zwoTemp.text())))
+		self.pb_setTemp.pressed.connect(lambda:self.main.threadclass.change_temp(float(self.ln_zwoTemp.text())))
 
 #-------------------------------------------------------------------------------#
 #Add option to return press line edits? Updates placeholder text.
 	def cool(self):
 		if self.pb_cooler.isChecked():
 			self.pb_cooler.setText("OFF")
-			main.threadclass.cooler_off() #hopefully this works.
+			self.main.threadclass.cooler_toggle(False) #hopefully this works.
 			print("ZWO cooler turned OFF")
 		else:
 			self.pb_cooler.setText("ON")
-			main.threadclass.cooler_on()
+			self.main.threadclass.cooler_toggle(True)
 			print("ZWO cooler turned ON")
+
+	def bit(self):
+		if self.rb_8bit.isChecked():
+			self.main.threadclass.bit(8)
+		elif self.rb_16bit.isChecked():
+			self.main.threadclass.bit(16)
+
+	def frametype(self):
+		if self.rb_light.isChecked():
+			self.main.threadclass.frametype_light()
+		elif self.rb_dark.isChecked():
+			self.main.threadclass.frametype_dark()
+		elif self.rb_flat.isChecked():
+			self.main.threadclass.frametype_flat()
+		elif self.rb_bias.isChecked():
+			self.main.threadclass.frametype_bias()
 
 	def updateTemp(self,data):
 		self.lbl_currentTemp_2.setText("%s C" %data[0])
-		self.lbl_currentPower.setText("%s \%" %data[1])
+		self.lbl_currentPower.setText("%s %%" %data[1])
+		if data[2] == True and not self.pb_cooler.isChecked(): 
+			print("Cooler on")
+			self.pb_cooler.toggle()
+			self.pb_cooler.setText("ON")
+		elif data[2] == False and self.pb_cooler.isChecked(): 
+			print("Cooler off")
+			self.pb_cooler.toggle()
+			self.pb_cooler.setText("OFF")
 	def updateConfig(self,data):
 		self.sb_binningx.setValue(data[0])
 		self.sb_binningy.setValue(data[1])
@@ -68,16 +94,12 @@ class ZWOCameraWindow(QtWidgets.QMainWindow, zwocamerawindow.Ui_ZWOcamera):
 		self.ln_framex.setPlaceholderText(str(data[7]))
 		self.ln_framey.setPlaceholderText(str(data[8]))
 	def updateConfig2(self,data):
-		#Set frame type
 		typ = {0:self.rb_light,1:self.rb_bias,2:self.rb_dark,3:self.rb_flat}
 		typ[data[1]].setChecked(True)
-		#Set cooler btn on/off 
-		if data[0] == 0 and self.pb_cooler.isChecked(): #if off turn on
-			self.pb_cooler.toggle()
-		elif data[0] == 1 and not self.pb_cooler.isChecked(): #if on turn off
-			self.pb_cooler.toggle()
 		#Set bit
-		self.rb_8bit.setChecked(True) if data[2] == 0 else self.rb_16bit.setChecked(True)
+		if data[2] == 8: self.rb_8bit.setChecked(True)
+		elif data[2] == 16: self.rb_16bit.setChecked(True)
+		else: print("ERROR: Unknown bit")
 
 #==============================================================================#
 
@@ -91,8 +113,9 @@ class TempThread(QtCore.QThread):
 		while 1:
 			temp = self.client.get_temp()
 			cpower = self.client.get_cooler_power()
-			self.signal.emit([temp,cpower])
-			time.sleep(3)
+			ctoggle = self.client.cooler_status()
+			self.signal.emit([temp,cpower,ctoggle])
+			time.sleep(5)
 
 class ConfigThread(QtCore.QThread):
 	signal = pyqtSignal('PyQt_PyObject')
