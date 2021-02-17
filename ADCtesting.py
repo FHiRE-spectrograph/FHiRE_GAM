8a74dc1ab1ab94eec44b982f1c3b972756326522
FHiRE <fhire@fhire_GAM.uwyo.edu>
2021-02-17 16:42:02 -0700

    Working version of ADC control. Tested based off Filippenko 1982 parameters so will need to be optimized with our focal length, distance between prisms and focus, and with WIRO conditions.

--- ADCtesting.py
+++ ADCtesting.py
@@ -1,6 +1,10 @@
+# 
+# Updated: 02/11/21
+#
 from PyQt5 import QtGui,QtCore,QtWidgets,uic
 from PyQt5.QtCore import QThread,pyqtSignal
 import time, math
+import numpy as np
 import RPi.GPIO as gpio
 
 import adcwindow #imports PyQt ADC testing window
@@ -44,12 +48,19 @@ top_switch = switch(38,35) #microswitch out, in
 bottom_switch = switch(37,31) 
 
 class ADCTestingWindow(QtWidgets.QMainWindow, adcwindow.Ui_ADC):
+	#Widget definitions
 	def __init__(self,main):
 		self.main = main
 		super(ADCTestingWindow,self).__init__(main)
 		self.setupUi(self)
 
 		self.pb_adcHome.pressed.connect(self.main.adcthread.ADC_home) 
+
+		self.afocus1_lineEdit.returnPressed.connect(lambda:self.main.adcthread.move_top(self.afocus1_lineEdit.text()))
+		self.afocus2_lineEdit.returnPressed.connect(lambda:self.main.adcthread.move_bottom(self.afocus1_lineEdit.text()))
+
+		self.afocus1_btn_sub.pressed.connect(self.main.adcthread.calc_steps_top)
+		self.afocus2_btn_sub.pressed.connect(self.main.adcthread.calc_steps_bottom)
 
 	def adc_top(self,top):
 		self.top = top
@@ -67,11 +78,8 @@ class ADCThread(QtCore.QThread):
 	sig1 = pyqtSignal('PyQt_PyObject')
 	sig2 = pyqtSignal('PyQt_PyObject')
 	
-	def __init__(self,main):
-		self.main = main
-		super(ADCThread,self).__init__(main)
-		
-		self.j = 0; self.k = 0
+	def __init__(self,parent=None):
+		super(ADCThread,self).__init__(parent)
 
 		self.telra = 1
 		self.teldec = 1
@@ -79,27 +87,30 @@ class ADCThread(QtCore.QThread):
 		self.teltemp = 1
 		self.telutc = 1
 		self.telhad = 1
-		self.z = 1
+		self.telP = 1
+		self.telairm = 1
+		self.z = 1		
+
+		stepper3.disable() 
+		stepper2.disable()
 
 	def run(self):
 		time.sleep(8)
 		self.ADC_home()
-		print("ADC set home finished")
+		print("ADC set home.")
 		time.sleep(6)
+
 		while 1:	
+			print("Updating ADC position.")
 			self.updatePosition()
-			print(self.teldec)
 			self.calc_steps_top()
-			#self.move_steps_top()
 			self.calc_steps_bottom()
-			#self.move_steps_bottom()
-			print("Here")
-			time.sleep(10)
+			print("ADC update complete.")
+			time.sleep(30)
 
 	#------------Top prism----------------------#
-	#finds parallactic angle and aligns top prism to parallactic 
-	#angle and bottom prism to zero correction position
-	def calc_steps_top(self):
+	#finds parallactic angle and aligns top and bottom prism to parallactic angle
+	def calc_steps_top(self): 
 		obslat = 41.097056 #[degrees]
 		had_r = math.radians(float(self.telhad))
 		obslat_r = math.radians(float(obslat))
@@ -107,100 +118,159 @@ class ADCThread(QtCore.QThread):
 
 		print("Dec: %s" %dec_r)
 		
-		#finds sin of parallactic angle
-		sinpar = (math.sin(had_r)*math.cos(obslat_r))/(1.-(math.sin(obslat_r)*math.sin(dec_r)+math.cos(obslat_r)*math.cos(dec_r)*math.cos(had_r))**2)**0.5	
+		#finds tan of parallactic angle
+		#equation from https://sites.astro.caltech.edu/~mcs/CBI/pointing/ and Egner et al. 2010
+		tanpar = (math.sin(had_r))/((math.tan(obslat_r)*math.cos(dec_r))-(math.sin(dec_r)*math.cos(had_r)))	
 
 		#gets parallactic angle in degrees
-		par_angle = math.degrees(math.asin(sinpar))
-		print("Parallactic: %s" %par_angle)
+		par_angle = math.degrees(math.atan(tanpar))
+		print("Parallactic angle (preflip): %s degrees" %par_angle)
 
-		#converts parallactic angle into motor steps (?)from home(?)
+		#have to add or subtract 180 depending on hour angle and dec
+		#if dec is higher than 41 degrees par angle should start at 180 instead of 0, and if HA is positive par values should be positive
+		#if dec is lower par angle should start at 0, and if HA is negative values are negative.
+		if dec_r > obslat_r:
+			if par_angle > 0 and had_r < 0:
+				par_angle -= 180
+			elif par_angle <= 0 and had_r >= 0:
+				par_angle += 180
+
+		#add 360 to negative par angles to ensure smooth prism rotation
+		if dec_r > obslat_r and par_angle < 0:
+			par_angle += 360
+
+		print("Parallactic angle (postflip): %s degrees" %par_angle)
+
+		#converts parallactic angle into motor steps from home
 		parstepsfloat = par_angle/.315 
 		self.parstepsint = int(parstepsfloat)
-		print("Top Prism move: %s steps" %self.parstepsint)
+
+		#calculates rotation angle to move both top and bottom prism to account for dispersion at a given zenith angle, temperature, and pressure
+		#0.38 - 0.68 microns range of FHiRE
+		lam_min = 0.38 #[microns]
+		lam_max = 0.68 #[microns]
+		
+		#equations (from https://refractiveindex.info/) to determine index of refraction for BaK2 and CaF2 prisms 
+		#n1= BaK2, n2 = CaF2
+		a0 = 2.319725
+		a1 = 0.002602493
+		a2 = 0.02249807
+		a3 = -0.002672041
+		a4 = 0.0003926686
+		a5 = -2.043303*10**-5
+
+		n1_min = np.sqrt(a0+(a1*lam_min**2)+(a2*lam_min**(-2))+(a3*lam_min**(-4))+(a4*lam_min**(-6))+(a5*lam_min**(-8)))
+		n1_max = np.sqrt(a0+(a1*lam_max**2)+(a2*lam_max**(-2))+(a3*lam_max**(-4))+(a4*lam_max**(-6))+(a5*lam_max**(-8)))
+
+		n1 = n1_max - n1_min
+
+		b0 = 0.443749998
+		b1 = 0.444930066
+		b2 = 0.150133991
+		b3 = 8.85319946
+		b4 = 0.00178027854
+		b5 = 0.00788536061
+		b6 = 0.0124119491
+		b7 = 2752.28175
+
+		n2_min = (b0*lam_min**2)/(lam_min**2-b4) + (b1*lam_min**2)/(lam_min**2-b5) + (b2*lam_min**2)/(lam_min**2-b6) + (b3*lam_min**2)/(lam_min**2-b7)
+		n2_min = np.sqrt(n2_min + 1)
+		n2_max = (b0*lam_max**2)/(lam_max**2-b4) + (b1*lam_max**2)/(lam_max**2-b5) + (b2*lam_max**2)/(lam_max**2-b6) + (b3*lam_max**2)/(lam_max**2-b7)
+		n2_max = np.sqrt(n2_max + 1)
+
+		n2 = n2_max - n2_min
+
+		#equations from Filippenko 1982, Brescia et al. 2006, Tendulkar 2014
+		#calculating atmospheric refraction for range of wavelengths min to max
+		R_min_sealevel = float((64.328+29498.1/(146-lam_min**(-2))+255.4/(41-lam_min**(-2)))*10**-6)
+		R_min_scaled = float((R_min_sealevel)*self.telP*(1+(1.049-0.0157*self.teltemp)*self.telP*10**-6)/(720.883*(1+0.003661*self.teltemp)))
+
+		R_max_sealevel = float((64.328+29498.1/(146-lam_max**(-2))+255.4/(41-lam_max**(-2)))*10**-6)
+		R_max_scaled = float((R_max_sealevel)*self.telP*(1+(1.049-0.0157*self.teltemp)*self.telP*10**-6)/(720.883*(1+0.003661*self.teltemp)))
+
+		#atmospheric dispersion in radians
+		Delta = (self.telP/(760+(2.9*self.teltemp)))*(R_min_scaled-R_max_scaled)*np.tan(self.z)
+		print("Dispersion = %s rads" %Delta)
+
+		#compares value of atmospheric diffraction with needed value of correction and calculates rotation angle
+		F = 12700.3 # focal length in mm ***change once finalized in Zemax***
+		prismangle = math.radians(5.54) #prism angle
+		H = math.tan(prismangle) # normalized prism base
+		D = 367.79	# distance between prisms and focus mm ***change once finalized in Zemax***
+		dnglass1 = n1 - n2
+
+		Phi = np.degrees(np.arccos(np.tan(F*Delta/(D*dnglass1))/H))-90
+		print("Rotation angle = %s degrees" %Phi)
+		rotstepsfloat = Phi/.315
+		self.rotstepsint = int(rotstepsfloat)
+		#total steps=parallactic angle + dispersion compensation angle of rotation
+		self.top_steps = self.parstepsint + self.rotstepsint
+		
+		print("Top Prism move: %s steps" %self.top_steps)
+		
+		self.move_steps_top()
 		
 	def move_steps_top(self):
-		if self.top_abs > self.parstepsint:
-			while self.j > self.parstepsint:
+		stepper2.enable()
+		if self.top_abs > self.top_steps:
+			while self.top_abs > self.top_steps:
 				stepper2.set_direction(False) 
 				stepper2.step()
 				self.top_abs -= 1
-		elif self.top_abs < self.parstepsint:
-			while self.j < self.parstepsint:
+		elif self.top_abs < self.top_steps:
+			while self.top_abs < self.top_steps:
 				stepper2.set_direction(True)
 				stepper2.step()
 				self.top_abs += 1
 		
 		self.sig1.emit(self.top_abs)
 		stepper2.set_direction(True)
+		#disabling and enabling outputs prevent motors from getting too hot from idle current
+		stepper2.disable()
 
 	#----------------Bottom prism-------------------#
 	def calc_steps_bottom(self):
-		M = .02897 #[kg/mole]		
-		g = 9.80665 #[m/s**2]
-		R = 8.3144598 #[J/mol*K]
-		T_b = 288.15 #[kelvin]
-		lam_naught = 0.5 #[microns]
-		P_sea = 101325 #[pascals]
-		altitude = 2943 #[meters]
-		nglass = 1
-		lam = 1
-
-		#finds atmospheric differential dispersion relative to .5 microns
-		#in arc seconds as a function of pressure, temperature, humidity and zenith angle
-		P = float((P_sea*math.exp((-g*M*altitude)/(R*T_b)))/133.322)	#mmHg --- what is M?
-		#--#Hum_cor = ((0.0624-(0.00068/(lam**2)))/(1+(0.003661*T)))*hum   #scale the things
-		N_sea5 = float(((1/(10**6))*(64.328+(29498.1/(146-((1/lam_naught)**2)))+(255/(41-((1/lam_naught)**2)))))+1)
-		
-		N_WIRO5 = float(((N_sea5-1)*((P*(1+((1.049-(0.0157*self.teltemp))*(10**-6)*P)))/(720.883*(1+(0.003661*self.teltemp)))))+1)
-		N_sea = float(((1/(10**6))*(64.328+(29498.1/(146-((1/lam)**2)))+(255/(41-((1/lam)**2)))))+1)
-		N_WIRO = float(((N_sea5-1)*((P*(1+((1.049-(0.0157*self.teltemp))*(10**-6)*P)))/(720.883*(1+(0.003661*self.teltemp)))))+1)
-		Delta = float((N_WIRO-N_WIRO5)*math.tan(self.z))
-
-		#compares value of atmospheric diffraction with needed value of correction and displaces bottom prism
-		F = 14925 # focal length in mm
-		prismangle = math.radians(15) #***Added temp.***
-		H = math.tan(prismangle) # normalized prism base
-		D = 269	# distance between prisms and focus mm
-		nglass15 = 1.4623
-		nglass1lam = nglass
-		dnglass1 = nglass15-nglass1lam
-		Fee = math.degrees(math.acos((1/H)*math.tan((F*Delta)/(D*(dnglass1))))) 
-		stepsfloat = Fee/.315
-		stepsint = int(stepsfloat)
-		self.bot_steps = self.top_abs+stepsint
+		#bottom steps=top steps - 2*dispersion compensation angle of rotation (since already subtracted from top)
+		self.bot_steps = self.top_abs - (2*self.rotstepsint)
 		print("Bottom Prism move: %s steps" %self.bot_steps)
+
+		self.move_steps_bottom()
+
+	def move_top(self,steps):
+		self.top_steps = steps
+	def move_bot(self,steps):
+		self.bot_steps = steps
 
 	def move_steps_bottom(self):
 		print("bottom steps: %s" %self.bot_abs)
-		
-		if self.k > self.bot_steps:
-			while self.k > self.bot_steps:
-				stepper3.set_direction(False)
-				stepper3.step()
-				self.k -= 1
-				self.bot_abs -= 1
-		elif self.k < self.bot_steps:
-			while self.k < self.bot_steps:
+		stepper3.enable()
+		if self.bot_abs > self.bot_steps:
+			while self.bot_abs > self.bot_steps:
 				stepper3.set_direction(True)
 				stepper3.step()
-				self.k += 1
+				self.bot_abs -= 1
+		elif self.bot_abs < self.bot_steps:
+			while self.bot_abs < self.bot_steps:
+				stepper3.set_direction(False)
+				stepper3.step()
 				self.bot_abs +=1
 		
-		print("k: %s" %self.k)
 		self.sig2.emit(self.bot_abs)
 		stepper3.set_direction(True)
+		stepper3.disable()
 
 	def updatePosition(self):
 		lat = math.radians(41.097)
 		lon = math.radians(105.977)
 
-		#Outside temperature in celsius
+		#Outside temperature in fahrenheit
 		fileopen = open("/home/fhire/Desktop/Telinfo","r")
 		self.telinfo = fileopen.read()
 		
 		teltemp = self.telinfo.split('\n')[66].split('>')[1].split('<')[0]
 		self.teltemp = float(teltemp.replace('\U00002013','-'))
+		#convert to celsius
+		self.teltemp = (5*self.teltemp/9 - 5*32/9)
 		#Outside humidity --- in percentage ***NEEDS TO BE IN mmhg***
 		self.telhum = float(self.telinfo.split('\n')[71].split('>')[1].split('<')[0])
 		#Hour Angle in degress
@@ -211,6 +281,11 @@ class ADCThread(QtCore.QThread):
 		self.telutc = self.telinfo.split('\n')[16].split('>')[1].split('<')[0]
 		#DEC in hour:minute:seconds
 		self.teldec = self.telinfo.split('\n')[31].split('>')[1].split('<')[0]
+		#Barometric pressure in inHg
+		self.telP = self.telinfo.split('\n')[76].split('>')[1].split('<')[0]
+		self.telP = float(self.telP)*25.4 #mmHg
+		#airmass
+		self.telairm = self.telinfo.split('\n')[46].split('>')[1].split('<')[0]
 		
 		#Convert RA to degrees
 		rahour = float(self.telra.split(":")[0])
@@ -224,20 +299,16 @@ class ADCThread(QtCore.QThread):
 		decseconds = float(self.teldec.split(":")[2])
 		self.teldec = dechour+decminute/60+decseconds/3600
 	
-		print("RA: %s\nDEC: %s\nUTC: %s\nHUM: %s\nTEMP: %s C\nHA: %s" %(self.telra,self.teldec,self.telutc,self.telhum,self.teltemp,self.telhad))
+		print("RA: %s\nDEC: %s\nUTC: %s\nHUM: %s %%\nTEMP: %s C\nHA: %s\nP: %s mmHg\nAirmass: %s" %(self.telra,self.teldec,self.telutc,self.telhum,self.teltemp,self.telhad,self.telP, self.telairm))
 	
-		#Zenith calculation
-		t0 = math.radians(90-lat)
-		t1 = math.radians(90-RaD) 
-		p0 = math.radians(lon)
-		p1 = math.radians(RaD)
-		
-		zz = math.cos(t0)*math.cos(t1)+math.sin(t0)*math.sin(t1)*math.cos(p1-p0)
-		self.z = math.acos(zz) #zenith angle [radians]
+		self.z = math.acos(1/float(self.telairm))
+		print("z: %s" %self.z)
 		
 		fileopen.close()
 		
 	def ADC_home(self):
+		stepper3.enable()
+		stepper2.enable()
 		bottom_switch.pin_start()
 		time.sleep(0.05)
 		if gpio.input(31) == 1: #send bottom ADC home
@@ -245,14 +316,17 @@ class ADCThread(QtCore.QThread):
 				stepper3.step()
 				time.sleep(0.05)
 		bottom_switch.pin_stop()
+
 		top_switch.pin_start()
 		if gpio.input(35) == 1: #send top ADC home
 			while gpio.input(35) == 1:
 				stepper2.step()
 				time.sleep(0.05)
 
-		print("ADC sent home")
+		#print("ADC sent home")
 		top_switch.pin_stop()
+		stepper3.disable()
+		stepper2.disable()
 
 		self.top_abs = 0
 		self.bot_abs = 0
