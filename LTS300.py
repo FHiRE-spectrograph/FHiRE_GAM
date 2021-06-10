bcbf091b1f1c485cd2ba367d82e6538e67bbc611
FHiRE <fhire@fhire_GAM.uwyo.edu>
2021-06-10 17:37:25 -0600

    Updated to work with the GAM GUI.

--- LTS300.py
+++ LTS300.py
@@ -1,90 +1,153 @@
-import time
-import serial
-from PyQt4 import QtCore, QtGui
+import time, serial, os, sys
+from PyQt5 import QtCore
+from PyQt5.QtCore import QThread,pyqtSignal
 
-class stage(QtCore.QObject):
-    def __init__(self):  
-	super(stage,self).__init__()
-        self.ser = serial.Serial(port = '/dev/ttyUSB0',
-                                 baudrate=115200,
-                                 bytesize=serial.EIGHTBITS,
-                                 parity=serial.PARITY_NONE,
-                                 stopbits=serial.STOPBITS_ONE,
-                                 rtscts=True)
-      
-        #Thorlabs recommends toggling the RTS pin
-        #and reseting the input and output buffers
-        #Is actually doing anything? Don't know...
-             
-        self.ser.setRTS(1)
-        time.sleep(0.05)
-        self.ser.flushInput()
-        self.ser.flushOutput()
-        time.sleep(0.05)
-        self.ser.setRTS(0)
-        
-        
-        #preset hex commands - see Thorlabs APT Communications Protocol
-        self.disable_motor = '\x10\x02\x01\x02\x21\x01'
-        self.enable_motor = '\x10\x02\x01\x02\x21\x01'
-        self.home_motor = '\x43\x04\x01\x00\x21\x01'
-        self.ser.write(self.home_motor)
-        
-        #last 4 bytes are the absolute position of the stage
-        #in hex signed two's complement 
-        
-        self.move_m_dist = '\x53\x04\x06\x00\x80\x01\x01\x00\x00\xC0\xF3\x00'
-        self.move_s_dist = '\x53\x04\x06\x00\x80\x01\x01\x00\x00\x40\xDB\x02'        
-        
-        #stops updates every 100 ms
-        self.stop_updates = '\x12\x00\x00\x00\x21\x01'
-        self.ser.write(self.stop_updates)
-        
-        #trying to add limit switch at 100 mm but does not work
-        #self.new_limits = '\x23\x04\x10\x00\x80\x01\x01\x00\x02\x00\x02\x00\xE8\xCC\xCC\x00\x00\x00\x00\x00\x03\x00'
-        #self.ser.write(self.new_limits)
-        
-        #sets max velocity to 20 mm/s and max accel to 5 mm/s
-        self.set_vel = '\x13\x04\x0E\x00\x80\x01\x01\x00\x00\x00\x00\x00\x02\x58\x00\x00\x00\x00\x36\x1A'
-        self.ser.write(self.set_vel)
-        
-        #sets home velocity to 5 mm/s
-        self.home_vel = '\x40\x04\x0E\x00\x80\x01\x01\x00\x00\x00\x00\x00\x00\x80\x8D\x06\x00\x00\x00\x00'
-        self.ser.write(self.home_vel)
-       
-        
-    def close(self):
-        self.ser.write(self.home_motor)
-        self.ser.close()
-        
-        
-    def reset(self):
-        self.ser.write(self.disable_motor)
-        time.sleep(1)
-        self.ser.write(self.enable_motor)
-        time.sleep(1)
-        self.ser.write(self.home_motor)
-        
-        
-    def enable(self):
-        self.ser.write(self.enable_motor)        
-        
-        
-    def disable(self):
-        self.ser.write(self.disable_motor)
-        
-        
-    def home(self):
-	print("Sending home")
-        self.ser.write(self.home_motor)     
-        
-        
-    def move_mirror(self):
-	print("Sending Mirror")
-        self.ser.write(self.move_m_dist)
-        
-        
-    def move_splitter(self,):
-	print("Sending Splitter")
-        self.ser.write(self.move_s_dist) 
-        
+# Class that communicates with LTS300.
+class Stage(QtCore.QObject):
+	signal_status = pyqtSignal('PyQt_PyObject')
+
+	# Preset hex commands - see Thorlabs APT Communications Protocol.
+	disable_motor = b'\x10\x02\x01\x02\x21\x01'
+	enable_motor = b'\x10\x02\x01\x01\x21\x01'
+	home_motor = b'\x43\x04\x01\x00\x21\x01'
+	home_str = '440401000150'
+
+	#
+	# Absolute position of the stage in hexadeximal two's complement
+	# use conversion.py to determine two's complement if changing positions
+	# change mirror and lens positions and strings here
+	# mirror position at 102 mm.
+	#
+	mirror_pos = b'\x00\x80\x7D\x02'
+	mirror_str = '00807d02'
+	# Beam splitter position at 5 mm.
+	splitter_pos = b'\x00\x40\x1F\x00'   
+	splitter_str = '00401f00'
+
+	# Move command consists of base move hex plus the move position in 
+	# two's complement.
+	move = b'\x53\x04\x06\x00\x80\x01\x01\x00'
+	move_mirror = move + mirror_pos
+	move_splitter = move + splitter_pos
+
+	# Sets max velocity to 10 mm/s and max accel to 5 mm/s.
+	setvel = b'\x13\x04\x0E\x00\x80\x01\x01\x00\x00\x00\x00\x00\x02\x58\x00\x00\x00\x00\x1B\x0D'
+
+	# Sets home velocity to 5 mm/s.
+	homevel = b'\x40\x04\x0E\x00\x80\x01\x01\x00\x00\x00\x00\x00\x00\x80\x8D\x06\x00\x00\x00\x00' 
+
+	def __init__(self, parent=None):
+		super(self.__class__, self).__init__(parent)
+		#
+		# Make sure not to plug in stage/filter USBs while RPi is ON.
+		# The QHY filterwheel (connected through USB on the ZWO camera)
+		# defaults to ttyUSB0 at restart. Stage defaults to the next USB
+		# designation, ttyUSB1. Plugging in these USBs while the
+		# RPi is ON sets its designation to USB0, USB1 in terms of order 
+		# plugged in rather than the expected restart default. It looks
+		# like it doesn't matter which port the stage, camera, 
+		# filterwheel USBs are connected on the GAM RPi.
+		#
+		self.ser = serial.Serial(
+				port = '/dev/ttyUSB1',
+				baudrate=115200,
+				bytesize=serial.EIGHTBITS,
+				parity=serial.PARITY_NONE,
+				stopbits=serial.STOPBITS_ONE,
+				rtscts=True,
+				timeout=1)
+
+		# Thorlabs recommends toggling the RTS pin and resetting the 
+		# input and output buffers.
+		self.ser.setRTS(1)
+		time.sleep(0.05)
+		self.ser.flushInput()
+		self.ser.flushOutput()
+		time.sleep(0.05)
+		self.ser.setRTS(0)
+ 
+		# Set motor speeds.
+		self.write_msg(self.setvel) 
+		self.write_msg(self.homevel)
+		# Disable motor and turn off lights.
+		self.disable()
+
+		self.stop_thread = False
+
+	def run(self):
+		print("Starting thread")
+		#self.moveHome()		
+
+	# Disables printing.
+	def blockPrint(self):
+		sys.stdout = open(os.devnull, 'w')
+
+	# Enables printing.
+	def enablePrint(self):
+		sys.stdout = sys.__stdout__
+
+	# Sends messages that dont require a response.
+	def write_msg(self, message):
+		self.blockPrint()
+		time.sleep(1)
+		self.ser.write(message)
+		self.enablePrint()
+
+	# Sends move messages that require a response and waits.
+	def move_msg(self, message):
+		print("Moving stage.")
+		self.signal_status.emit('busy')
+		self.blockPrint()
+		time.sleep(1)
+		self.ser.write(message)
+		self.enablePrint()
+		while 1:
+			msg = self.ser.readline()
+			if msg == b'':
+				pass
+			else:
+				hex_msg = msg.hex()
+				self.signal_status.emit(hex_msg)
+				break
+			if self.stop_thread == True:
+				print("Stage disconnected while attempting to move.")
+				break
+
+	# Command functions.
+	def enable(self):
+		self.write_msg(self.enable_motor)        
+
+	def disable(self):
+		self.write_msg(self.disable_motor)
+
+	def reset(self):
+		self.disable()
+		time.sleep(1)
+		self.enable()
+		time.sleep(1)
+		self.move_msg(self.home_motor)
+
+	def moveHome(self):
+		print("Stage: Moving home")
+		self.enable()
+		self.move_msg(self.home_motor)
+		self.disable()
+
+	def moveMirror(self):
+		self.enable()
+		self.move_msg(self.move_mirror)
+		self.disable()
+
+	def moveSplitter(self):
+		self.enable()		
+		self.move_msg(self.move_splitter)
+		self.disable()
+
+	def stop(self, exit): 
+		# Send stage to mirror at closedown to protect lenses. 
+		if True:
+			self.moveMirror()
+		self.stop_thread = True
+		self.ser.close()
+
+
